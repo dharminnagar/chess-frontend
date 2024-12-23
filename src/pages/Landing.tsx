d05c4698290040653901efc39383e2bb100b6091
Dharmin Nagar <dharminnagar@gmail.com>
2024-12-23 17:06:33 +0530

    v1 of Landing Page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,18 @@
 export function Landing() {
   return (
-    <div className="w-screen h-screen bg-slate-700 text-white">
-      <div className="flex flex-col items-center justify-center h-full">
-        <div className="text-4xl">Hello, Welcome to Chess app</div>
-        <button className="bg-green-700 p-2 rounded-md">Join Room</button>
+    <div className="w-screen h-screen bg-gradient-to-bl from-slate-900 to-black text-white">
+      <div className="flex justify-center items-center h-full">
+        <div className="flex justify-around items-center gap-28">
+          <img
+            src="../../public/chessboard.png"
+            alt="Chess Board"
+            className="h-[90vh]"
+          />
+          <div className="flex flex-col items-center justify-center gap-5 h-full">
+            <div className="text-4xl">Hello, Welcome to Chess app</div>
+            <button className="bg-green-700 p-2 rounded-md">Join Room</button>
+          </div>
+        </div>
       </div>
     </div>
   );
