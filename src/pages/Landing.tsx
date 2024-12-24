75822ef6dc07b2b5da465c011ca55d9742a961f8
Dharmin Nagar <dharminnagar@gmail.com>
2024-12-24 07:52:00 +0530

    added route handle

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 export function Landing() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-screen h-screen bg-gradient-to-bl from-slate-900 to-black text-white">
       <div className="flex justify-center items-center h-full">
@@ -10,7 +14,12 @@ export function Landing() {
           />
           <div className="flex flex-col items-center justify-center gap-5 h-full">
             <div className="text-4xl">Hello, Welcome to Chess app</div>
-            <button className="bg-green-700 p-2 rounded-md">Join Room</button>
+            <button
+              className="bg-green-700 p-2 rounded-md"
+              onClick={() => navigate("/game")}
+            >
+              Join Room
+            </button>
           </div>
         </div>
       </div>
