80fc04b3d305c5abf04dda6521740ccc0f11fed9
Dharmin Nagar <dharminnagar@gmail.com>
2024-12-24 09:30:32 +0530

    added global style

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import { Game } from "./pages/Game";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Add your routes here */}
-        <Route path="/" element={<Landing />} />
-        <Route path="/game" element={<Game />} />
+    <div className="w-screen h-screen bg-gradient-to-bl from-slate-900 to-black text-white">
+      <BrowserRouter>
+        <Routes>
+          {/* Add your routes here */}
+          <Route path="/" element={<Landing />} />
+          <Route path="/game" element={<Game />} />
 
-        {/* This is the spectator endpoint */}
-        {/* <Route path="/room/:id" element={<Room />} /> */}
-      </Routes>
-    </BrowserRouter>
+          {/* This is the spectator endpoint */}
+          {/* <Route path="/room/:id" element={<Room />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
