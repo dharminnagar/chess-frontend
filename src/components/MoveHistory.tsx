26f2e22e43f32f589d9d49f663310e72beb99324
Dharmin Nagar <dharminnagar@gmail.com>
2024-12-26 11:36:38 +0530

    fixed Move History overflowing

--- src/components/MoveHistory.tsx
+++ src/components/MoveHistory.tsx
@@ -1,14 +1,18 @@
 import { Chess } from "chess.js";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function MoveHistory({ chess }: { chess: Chess }) {
   const [moves, setMoves] = useState<string[]>(chess.history());
+  const scrollRef = useRef<HTMLDivElement>(null);
   const updateMoves = () => setMoves(chess.history());
 
   useEffect(() => {
     updateMoves();
   }, [chess.history().length]);
-  console.log("Moves", moves);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [moves]);
 
   if (moves.length === 0) {
     return (
@@ -20,15 +24,17 @@ export function MoveHistory({ chess }: { chess: Chess }) {
   }
 
   return (
-    <div className="">
-      <h2 className="text-lg font-bold mb-2 col-span-3">Move History</h2>
-      {Array.from({ length: Math.ceil(moves.length / 2) }).map((_, i) => (
-        <div key={i} className="grid grid-cols-6 text-s">
-          <div className="col-span-1">{i + 1}.</div>
-          <div className="col-span-2">{moves[i * 2] || ""}</div>
-          <div className="col-span-2">{moves[i * 2 + 1] || ""}</div>
-        </div>
-      ))}
+    <div>
+      <div className="text-lg font-bold mb-2 col-span-3">Move History</div>
+      <div className="max-h-96 overflow-y-auto" ref={scrollRef}>
+        {Array.from({ length: Math.ceil(moves.length / 2) }).map((_, i) => (
+          <div key={i} className="grid grid-cols-6 text-s">
+            <div className="col-span-1">{i + 1}.</div>
+            <div className="col-span-2">{moves[i * 2] || ""}</div>
+            <div className="col-span-2">{moves[i * 2 + 1] || ""}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
